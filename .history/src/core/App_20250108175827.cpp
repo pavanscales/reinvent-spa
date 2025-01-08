206317bfc02eec94f67220f56287e0005641c810
xiter22222 <pawanpediredla@gmail.com>
2025-01-08 17:58:54 +0530

    Add core application logic (App.cpp, App.h).

---
+++ .history/src/core/App_20250108175827.cpp
@@ -0,0 +1,15 @@
+
+/*
+  This code belongs to Pawvan.
+  It is part of the stripe_api_integration_wrapper project.
+  Licensed under MIT.
+*/
+
+#include "App.h"
+void App ::run(){
+    
+}
+class App{
+    public:
+    void run()
+}
